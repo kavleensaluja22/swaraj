3667277c32e7234ff4dfd5745f14257125d64bb8
kavleen <kavleensaluja22@gmail.com>
2025-06-06 21:49:48 +0530

    deployed

--- appname/utils.py
+++ appname/utils.py
@@ -1,27 +1,27 @@
-from django.urls import reverse
-from twilio.rest import Client
-import random
-from .models import OTP
+# from django.urls import reverse
+# from twilio.rest import Client
+# import random
+# from .models import OTP
 
-account_sid = 'AC1abd6b5767a3a8171f3ae628cf2a1fef'
-auth_token = 'c987e6c19c60f6736442da61836c5e02'
-client = Client(account_sid, auth_token)
-
-
-def generate_otp():
-    return random.randint(100000, 999999)
+# account_sid = 'AC1abd6b5767a3a8171f3ae628cf2a1fef'
+# auth_token = 'c987e6c19c60f6736442da61836c5e02'
+# client = Client(account_sid, auth_token)
 
 
-def send_otp(phone_number):
-    try:
-        otp = generate_otp()
-        message = client.messages.create(
-            body=f"SWARAJ TRACTOR HARYANA WELCOMES YOU ,Your OTP is {otp}", from_="+13157132751", to=phone_number
-        )
+# def generate_otp():
+#     return random.randint(100000, 999999)
 
-        return otp , True
+
+# def send_otp(phone_number):
+#     try:
+#         otp = generate_otp()
+#         message = client.messages.create(
+#             body=f"SWARAJ TRACTOR HARYANA WELCOMES YOU ,Your OTP is {otp}", from_="+13157132751", to=phone_number
+#         )
+
+#         return otp , True
         
-    except Exception as e:
-        print(e)
+#     except Exception as e:
+#         print(e)
     
-    return '' , False
+#     return '' , False
--- appname/views.py
+++ appname/views.py
@@ -1,33 +1,16 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import path
-from django.contrib.auth import authenticate,login
-import os
-from appname.models import Feedback
-from datetime import datetime
-from django.contrib.auth.models import User
+from django.urls import path, reverse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from django.contrib.auth import login, logout
 from django.conf import settings
-from django.shortcuts import render, redirect , HttpResponse
-from django.contrib.auth import logout
-from appname.models import GalleryImage
 from django.http import JsonResponse
-import random
-from django.shortcuts import render
-from twilio.rest import Client
-from .models import Feedback
-import appname
-from django.urls import reverse
-import json
-from django.views.decorators.csrf import csrf_exempt
-from .utils import send_otp
-from django.shortcuts import render, redirect
 from django.contrib import messages
-# from twilio.rest import Client
-import random
-from .models import OTP
-from django.shortcuts import get_object_or_404
-from django.core.exceptions import ValidationError
+from django.views.decorators.csrf import csrf_exempt
 
+from datetime import datetime
+import json
 
+from .models import Feedback, OTP, GalleryImage
+# from .utils import send_otp
 
 
 def home(request):
@@ -47,8 +30,7 @@ def home(request):
 
 
 def location(request):
-    return render (request ,'location.html')
-
+    return render(request, 'location.html')
 
 
 def feedback(request):
@@ -67,42 +49,26 @@ def feedback(request):
     return render(request, 'feedback.html')
 
 
-
 def gallery(request):
-    if request.method=="GET":
+    if request.method == "GET":
         images = GalleryImage.objects.all()
-        return render(request,'photos.html',{'images':images})
-
-
-
+        return render(request, 'photos.html', {'images': images})
 
 
 def g_try(request):
-    return render(request,'g_try.html')
+    return render(request, 'g_try.html')
 
-def s724(request):
-    return render(request,'s724.html')
-def s733(request):
-    return render(request,'s733.html')
-def s735(request):
-    return render(request,'s735.html')
-def s744(request):
-    return render(request,'s744.html')
-def s855(request):
-    return render(request,'s855.html')
-def s969(request):
-    return render(request,'s969.html')
-def s963(request):
-    return render(request,'s963.html')
-def s717(request):
-    return render(request, 's717.html')
-def combine1(request):
-    return render(request,'combine1.html')
-def combine2(request):
-    return render(request,'combine2.html')
-def combine3(request):
-    return render(request,'combine3.html')
-def index(request):
-    return render(request,'index.html')
-def xt(request):
-    return render(request,'xt.html')
+
+def s724(request): return render(request, 's724.html')
+def s733(request): return render(request, 's733.html')
+def s735(request): return render(request, 's735.html')
+def s744(request): return render(request, 's744.html')
+def s855(request): return render(request, 's855.html')
+def s969(request): return render(request, 's969.html')
+def s963(request): return render(request, 's963.html')
+def s717(request): return render(request, 's717.html')
+def combine1(request): return render(request, 'combine1.html')
+def combine2(request): return render(request, 'combine2.html')
+def combine3(request): return render(request, 'combine3.html')
+def index(request): return render(request, 'index.html')
+def xt(request): return render(request, 'xt.html')
