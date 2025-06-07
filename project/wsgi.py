dc8814f96c7de2d3d27f188df68ce27fbf607f6d
kavleen <kavleensaluja22@gmail.com>
2025-06-07 13:12:25 +0530

    settings

--- project/wsgi.py
+++ project/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
