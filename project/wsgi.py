836bbd0f889c78c2dc2d7be0e162de64be631ec9
kavleen <kavleensaluja22@gmail.com>
2025-06-07 13:25:19 +0530

    settings

--- project/wsgi.py
+++ project/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-
+from django.conf import settings 
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
@@ -14,4 +14,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
